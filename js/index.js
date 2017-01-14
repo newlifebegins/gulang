21334585aebdca3c41615d5581beb5e03552ad2c
weiwang <1345795498@qq.com>
2017-01-14 15:44:15 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	//轮播图
+	var mySwiper = new Swiper('.swiper-container',{
+		pagination : '.swiper-pagination',
+	 	speed:100,
+		autoplay:300,
+		autoplayDisableOnInteraction:false,
+		loop : true,//循环
+		observer:true,
+        observeParents:true,
+		paginationClickable :true,//控制器
+	})
+
+	// 一站式服务轮播图
+	var num=1;
+	function move(){
+	      if(num==5){
+	            num=0;
+	      }
+	      $('ul.ne li').hide();
+	      $(".wenzi ul.xuhao li").removeClass('active');
+	      $('ul.ne li').eq(num).show();
+	      $(".wenzi ul.xuhao li").eq(num).addClass('active');
+	      num++;
+	}      
+	var t=setInterval(move,2000);
+	var index;
+	$(".wenzi ul.xuhao li").hover(function(){
+		clearInterval(t);
+		index = $(this).index();
+		$(this).addClass("active").siblings("li").removeClass("active");
+		var _this = $(this).parent().siblings("ul.ne");
+		_this.find("li").eq(index).show().siblings("li").hide();
+	},function(){
+		num = index + 1;
+		t=setInterval(move,2000);
+	})
+
+
+	$("nav ul li.indexone").click(function(){
+		window.location.href="index.html";
+	})
+	
+	$("nav ul li ul li").each(function(){
+		var alen = $(this).find("a").length;
+		if(alen==2){
+			$(this).css("width",260+"px");
+			$(this).parent().css("width",260+"px");
+		}
+	})
+	
+})
+function scroll(obj) {
+	var tmp = (obj.scrollLeft)++;
+	if (obj.scrollLeft==tmp) obj.innerHTML += obj.innerHTML;
+	if (obj.scrollLeft>=obj.firstChild.offsetWidth) obj.scrollLeft=0;
+}
+var timer =	setInterval("scroll(document.getElementById('scrollobj'))",30);
+
